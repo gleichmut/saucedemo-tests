0cb37f7bcb2e95a23499e4f810f0c0a073456fce
Your Name <your_felicia@mail.ru>
2026-02-01 19:00:53 +0300

    Added BaseTest.java with setup and teardown

---
+++ src/test/java/base/BaseTest.java
@@ -0,0 +1,25 @@
+package base;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public abstract class BaseTest {
+
+    protected WebDriver driver;
+
+    @BeforeEach
+    void setUp() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.saucedemo.com/");
+    }
+
+    @AfterEach
+    void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
